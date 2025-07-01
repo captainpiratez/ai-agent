c09095a3e9e98426792c05a6274b238d0337b4d7
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-07-02 02:39:50 +0300

    changed pip to uv and added synced repo to latest files in boot.dev

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -13,7 +13,7 @@ def get_file_content(working_directory, file_path):
     try:
         with open(abs_file_path, "r") as f:
             content = f.read(MAX_CHARS)
-            if len(content) == MAX_CHARS:
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
                 content += (
                     f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
                 )
